05c6f2af06e5f310a3d1f5096879b7e6af12692e
ricardoalves1 <ras@ic.ufal.br>
2020-03-02 18:52:43 -0300

    commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import Smartphones from './Smartphones';
 import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import './Smartphones.css';
 
 export default class Form extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
           phones: [],
+          info: {"screens": [], "rams": [], "mems": [], "batteries": []},
           screen: '',
           ram: '',
           storage: '',
@@ -22,6 +25,30 @@ export default class Form extends React.Component {
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
+    componentDidMount() {
+      axios.get("https://smartphone-recommender-api.herokuapp.com/info")
+        .then(res => {
+          console.log(res.data)
+
+          let min = res.data.batteries[0] - res.data.batteries[0] % 500
+          let max = res.data.batteries[res.data.batteries.length -1]
+          let batteries = []
+
+          while(min <= max) {
+            batteries.push(min)
+            min += 500
+          }
+
+          res.data.batteries = batteries
+
+          this.setState({
+            info: res.data,
+          });
+        })
+        .catch(err => {
+          this.setState({erro: err})
+        })
+    }
 
     render() {
       let aux, acc;
@@ -42,60 +69,93 @@ export default class Form extends React.Component {
 
       return (
         <div>
-          <form onSubmit={this.handleSubmit}>
+          <form>
+            <div>
+              <TextField
+                label="Tamanho da tela (polegadas)"
+                select
+                className="input"
+                id="screen"
+                onChange={this.handleChange}
+                value={this.state.screen}
+                variant="outlined"
+                helperText="Por favor seleciona o tamanho da tela"
+              >
+                {this.state.info.screens.map(value => (
+                  <MenuItem key={value} id="screen" value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-            <TextField
-              label="Tamanho da tela (polegadas)"
-              variant="standard"
-              className="input"
-              id="screen"
-              onChange={this.handleChange}
-              value={this.state.screen}
-            />
+              <TextField
+                label="RAM (GB)"
+                select
+                className="input"
+                id="ram"
+                onChange={this.handleChange}
+                value={this.state.ram}
+                variant="outlined"
+                helperText="Por favor seleciona a RAM"
+              >
+                {this.state.info.rams.map(value => (
+                  <MenuItem key={value} id="ram" value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-            <TextField
-              label="RAM (GB)"
-              variant="standard"
-              className="input"
-              id="ram"
-              onChange={this.handleChange}
-              value={this.state.ram}
-            />
+              <TextField
+                label="Armazenamento (GB)"
+                select
+                className="input"
+                id="storage"
+                onChange={this.handleChange}
+                value={this.state.storage}
+                variant="outlined"
+                helperText="Por favor seleciona o armazenamento"
+              >
+                {this.state.info.mems.map(value => (
+                  <MenuItem key={value} id="storage" value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-            <TextField
-              label="Armazenamento (GB)"
-              variant="standard"
-              className="input"
-              id="storage"
-              onChange={this.handleChange}
-              value={this.state.storage}
-            />
-
-            <TextField
-              label="Bateria (mAh)"
-              variant="standard"
-              className="input"
-              id="battery"
-              onChange={this.handleChange}
-              value={this.state.battery}
-            />
-
-            <Button variant="contained" color="primary" type="submit" disableElevation>
-              Go
-            </Button>
-            <p>{this.state.erro}</p>
-
-            {this.state.brand}
-            {acc}
-            {aux}
+              <TextField
+                label="Bateria (mAh)"
+                select
+                className="input"
+                id="battery"
+                onChange={this.handleChange}
+                value={this.state.battery}
+                variant="outlined"
+                helperText="Por favor selecione a bateria"
+              >
+                {this.state.info.batteries.map(value => (
+                  <MenuItem key={value} id="battery" value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
+              </TextField>
+              
+              <Button variant="contained" color="primary" className="button" onClick={this.handleSubmit} disableElevation>
+                Go
+              </Button>
+            </div>
             
           </form>
+          <p>{this.state.erro}</p>
+
+          {this.state.brand}
+          {acc}
+          {aux}
         </div>
       );
     }
   
-    handleChange(e) {
-      let key = e.nativeEvent.target.id
+    async handleChange(e) {
+      let key = await e.nativeEvent.target.id
       this.setState({ [key]: e.target.value });
     }
   
@@ -105,8 +165,8 @@ export default class Form extends React.Component {
       if (
         !this.state.screen.length ||
         !this.state.ram.length ||
-        !this.state.storage.length ||
-        !this.state.battery.length
+        !this.state.storage ||
+        !this.state.battery
       ) {
         this.setState({erro: "Campo vazio"})
         this.state.isLoading = false
